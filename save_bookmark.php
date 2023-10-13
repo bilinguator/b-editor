857f3be65318912f28f4202dc2de2dd082aea894
Dmitrii Snitkin <snitkin.d@list.ru>
2023-10-14 02:13:54 +0300

    Replace tabulation with spaces.

--- save_bookmark.php
+++ save_bookmark.php
@@ -1,4 +1,4 @@
 <?php
-	header('Content-Type: text/html; charset=utf-8');
-	file_put_contents('books/bookmark.txt', $_POST['bookmark_number']);
+    header('Content-Type: text/html; charset=utf-8');
+    file_put_contents('books/bookmark.txt', $_POST['bookmark_number']);
 ?>
