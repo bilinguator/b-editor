3c8bccecba381c3f36464f10053015db54b40295
Dmitrii Snitkin <snitkin.d@list.ru>
2024-03-10 20:15:46 +0300

    Add different extensions for bookmarks according to extension of book 1.

--- save_bookmark.php
+++ save_bookmark.php
@@ -1,5 +1,5 @@
 <?php
     header('Content-Type: text/html; charset=utf-8');
-    $bookMarkPath = "books/bookmarks/bookmark_{$_POST['file_name_base']}.txt";
+    $bookMarkPath = "books/bookmarks/bookmark_{$_POST['file_name_base']}.{$_POST['file1_extension']}";
     file_put_contents($bookMarkPath, $_POST['bookmark_number']);
 ?>
